2bf4f43fabe89b910de47e631fa9ae3328aa20f9
Bijan <bijanbwb@gmail.com>
2016-01-30 12:07:11 -0500

    Feature for adding an unpersisted note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,28 +3,45 @@ import React from 'react';
 import Note from './Note.jsx';
 
 export default class App extends React.Component {
-  render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do Laundry'
-      }
-    ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const notes = this.state.notes;
     return (
       <div>
+        <button onClick={this.addNote}>+</button>
         <ul>{notes.map(note =>
           <li key={note.id}>{note.task}</li>
         )}</ul>
       </div>
     );
   }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New Task'
+      }])
+    });
+  };
 }
