5e19a53c93ee86143eb2d7fb69656ff6e3bf2ed9
Bijan <bijanbwb@gmail.com>
2016-01-30 10:02:14 -0500

    Resolve jsx files with babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ const PATHS = {
 
 const common = {
   entry: PATHS.app,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -18,6 +21,11 @@ const common = {
       {
         test: /\.css$/,
         loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDependency'],
         include: PATHS.app
       }
     ]
