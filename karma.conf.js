7622648f8090cd949c0b2c1ad25595f5d8a33b24
Bijan <bijanbwb@gmail.com>
2016-01-30 18:51:43 -0500

    Karma configuration file

---
+++ karma.conf.js
@@ -0,0 +1,54 @@
+// Reference: http://karma-runner.github.io/0.13/config/configuration-file.html
+module.exports = function karmaConfig (config) {
+  config.set({
+    frameworks: [
+      // Reference: https://github.com/karma-runner/karma-mocha
+      // Set framework to mocha
+      'mocha'
+    ],
+
+    reporters: [
+      // Reference: https://github.com/mlex/karma-spec-reporter
+      // Set reporter to print detailed results to console
+      'spec',
+      // Reference: https://github.com/karma-runner/karma-coverage
+      // Output code coverage files
+      'coverage'
+    ],
+
+    files: [
+      // Reference: https://www.npmjs.com/package/phantomjs-polyfill
+      // Needed because React.js requires bind and phantomjs does not support it
+      'node_modules/phantomjs-polyfill/bind-polyfill.js',
+      // Grab all files in the specs directory that contain _spec.
+      'specs/**/*_spec.*'
+    ],
+
+    preprocessors: {
+      // Reference: http://webpack.github.io/docs/testing.html
+      // Reference: https://github.com/webpack/karma-webpack
+      // Convert files with webpack and load sourcemaps
+      'specs/**/*_spec.*': ['webpack', 'sourcemap']
+    },
+
+    browsers: [
+      // Run tests using PhantomJS
+      'PhantomJS'
+    ],
+
+    singleRun: true,
+
+    coverageReporter: {
+      dir: 'build/coverage/',
+      type: 'html'
+    },
+
+    // Test webpack config
+    webpack: require('./webpack.config'),
+
+    // Hide webpack build information from output
+    webpackMiddleware: {
+      noInfo: true
+    }
+  });
+};
