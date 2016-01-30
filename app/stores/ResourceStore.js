133172bd74c9622e049fc0e885014cfadf60b8bf
Bijan <bijanbwb@gmail.com>
2016-01-30 17:57:55 -0500

    Add store code for create, update, and delete

--- app/stores/ResourceStore.js
+++ app/stores/ResourceStore.js
@@ -7,11 +7,32 @@ class ResourceStore {
     this.bindActions(ResourceActions);
     this.resources = [];
   }
+
   create(resource) {
+    const resources = this.resources;
+
+    resource.id = uuid.v4();
+    this.setState({
+      resources: resources.concat(resource)
+    });
   }
+
   update(updatedResource) {
+    const resources = this.resources.map(resource => {
+      if (resource.id === updatedResource.id) {
+        return Object.assign({}, resource, updatedResource);
+      }
+      return resource;
+    });
+
+    // This is same as `this.setState({resources: resources})`
+    this.setState({resources});
   }
+
   delete(id) {
+    this.setState({
+      resources: this.resources.filter(resource => resource.id !== id)
+    });
   }
 }
 
