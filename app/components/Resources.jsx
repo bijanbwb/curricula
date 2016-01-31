6a7ee603af9e488e5e8dd63c0c3eb5b4fc8b7010
Bijan <bijanbwb@gmail.com>
2016-01-31 13:45:04 -0500

    Switch out old Resource component for new one

--- app/components/Resources.jsx
+++ app/components/Resources.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Editable from './Editable.jsx';
+import Resource from './Resource.jsx';
 
 export default ({resources, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="resources">{resources.map(resource =>
-      <li className="resource" key={resource.id}>
-        <Editable
-          editing={resource.editing}
-          value={resource.task}
-          onValueClick={onValueClick.bind(null, resource.id)}
-          onEdit={onEdit.bind(null, resource.id)}
-          onDelete={onDelete.bind(null, resource.id)} />
-      </li>
+      <Resource className="resource" id={resource.id} key={resource.id}>
+      <Editable
+        editing={resource.editing}
+        value={resource.task}
+        onValueClick={onValueClick.bind(null, resource.id)}
+        onEdit={onEdit.bind(null, resource.id)}
+        onDelete={onDelete.bind(null, resource.id)} />
+      </Resource>
     )}</ul>
   );
 }
