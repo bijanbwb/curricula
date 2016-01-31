8baf089ab556f1ebcac5a1ac19c9e5085d10b959
Bijan <bijanbwb@gmail.com>
2016-01-30 21:10:38 -0500

    Remove resources and add paths for now

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,36 +1,28 @@
 import uuid from 'node-uuid';
 import AltContainer from 'alt-container';
 import React from 'react';
-import Resources from './Resources.jsx';
-import ResourceActions from '../actions/ResourceActions';
-import ResourceStore from '../stores/ResourceStore';
+import Paths from './Paths.jsx';
+import PathActions from '../actions/PathActions';
+import PathStore from '../stores/PathStore';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <button className="add-resource" onClick={this.addResource}>+</button>
+        <button className="add-path" onClick={this.addPath}>+</button>
         <AltContainer
-          stores={[ResourceStore]}
+          stores={[PathStore]}
           inject={{
-            resources: () => ResourceStore.getState().resources
+            paths: () => PathStore.getState().paths || []
           }}
         >
-          <Resources onEdit={this.editResource} onDelete={this.deleteResource} />
+          <Paths />
         </AltContainer>
       </div>
     );
   }
 
-  addResource() {
-    ResourceActions.create({task: 'New task'});
-  }
-
-  editResource(id, task) {
-    ResourceActions.update({id, task});
-  }
-
-  deleteResource(id) {
-    ResourceActions.delete(id);
+  addPath() {
+    PathActions.create({name: 'New Path'});
   }
 }
