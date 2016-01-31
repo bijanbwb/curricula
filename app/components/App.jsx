abbce5f8fde4e8c7d0fd45eb8bf10636653f626b
Bijan <bijanbwb@gmail.com>
2016-01-30 20:13:11 -0500

    Refactoring with AltContainer

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,40 +1,23 @@
 import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
 import Resources from './Resources.jsx';
 import ResourceActions from '../actions/ResourceActions';
 import ResourceStore from '../stores/ResourceStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = ResourceStore.getState();
-  }
-
-  componentDidMount() {
-    ResourceStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    ResourceStore.unlisten(this.storeChanged);
-  }
-
-  storeChanged = (state) => {
-    // Without a property initializer `this` wouldn't
-    // point at the right context because it defaults to
-    // `undefined` in strict mode.
-    this.setState(state);
-  };
-
   render() {
-    const resources = this.state.resources;
-
     return (
       <div>
         <button className="add-resource" onClick={this.addResource}>+</button>
-        <Resources resources={resources}
-          onEdit={this.editResource}
-          onDelete={this.deleteResource} />
+        <AltContainer
+          stores={[ResourceStore]}
+          inject={{
+            resources: () => ResourceStore.getState().resources
+          }}
+        >
+          <Resources onEdit={this.editResource} onDelete={this.deleteResource} />
+        </AltContainer>
       </div>
     );
   }
