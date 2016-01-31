43988c928bd17b23bb213ca4f6539dac897cccb3
Bijan <bijanbwb@gmail.com>
2016-01-30 22:03:27 -0500

    Lane editing with console.log()

--- app/components/Path.jsx
+++ app/components/Path.jsx
@@ -4,6 +4,7 @@ import Resources from './Resources.jsx';
 import ResourceActions from '../actions/ResourceActions';
 import ResourceStore from '../stores/ResourceStore';
 import PathActions from '../actions/PathActions';
+import Editable from './Editable.jsx';
 
 export default class Path extends React.Component {
   constructor(props) {
@@ -11,18 +12,23 @@ export default class Path extends React.Component {
     const id = props.path.id;
     this.addResource = this.addResource.bind(this, id);
     this.deleteResource = this.deleteResource.bind(this, id);
+    this.editName = this.editName.bind(this, id);
+    this.deletePath = this.deletePath.bind(this, id);
+    this.activatePathEdit = this.activatePathEdit.bind(this, id);
   }
 
   render() {
     const {path, ...props} = this.props;
     return (
       <div {...props}>
-        <div className="path-header">
+        <div className="path-header" onClick={this.activatePathEdit}>
           <div className="path-add-resource">
             <button onClick={this.addResource}>+</button>
           </div>
-          <div className="path-name">
-            {path.name}
+          <Editable className="path-name" editing={path.editing}
+            value={path.name} onEdit={this.editName} />
+          <div className="path-delete">
+             <button onClick={this.deletePath}>x</button>
           </div>
         </div>
         <AltContainer
@@ -31,13 +37,18 @@ export default class Path extends React.Component {
             resources: () => ResourceStore.getResourcesByIds(path.resources)
           }}
         >
-          <Resources onEdit={this.editResource} onDelete={this.deleteResource} />
+          <Resources
+           onValueClick={this.activateResourceEdit}
+           onEdit={this.editResource}
+           onDelete={this.deleteResource} />
         </AltContainer>
       </div>
     );
   }
 
   addResource(pathId, e) {
+    e.stopPropagation();
+
     const resource = ResourceActions.create({task: 'New Task'});
 
     PathActions.attachToPath({
@@ -53,5 +64,21 @@ export default class Path extends React.Component {
   deleteResource(pathId, resourceId) {
     PathActions.detachFromPath({pathId, resourceId});
     ResourceActions.delete(resourceId);
+  }
+
+  editName(id, name) {
+    console.log(`edit path ${id} name using ${name}`);
+  }
+
+  deletePath(id) {
+    console.log(`delete path ${id}`);
+  }
+
+  activatePathEdit(id) {
+    console.log(`activate path ${id} edit`);
+  }
+
+  activateResourceEdit(id) {
+    console.log(`activate resource ${id} edit`);
   }
 }
