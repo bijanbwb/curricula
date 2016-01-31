0e835b0410f1728a7ea65b32e1fdbbbadc2a4c2f
Bijan <bijanbwb@gmail.com>
2016-01-31 13:58:10 -0500

    Remove weird newline chars from Atom and copy-paste

--- app/components/Resource.jsx
+++ app/components/Resource.jsx
@@ -1,1 +1,23 @@
-import React from 'react';import {DragSource} from 'react-dnd';import ItemTypes from '../constants/itemTypes';const resourceSource = {  beginDrag(props) {    console.log('begin dragging resource', props);    return {};  }};@DragSource(ItemTypes.RESOURCE, resourceSource, (connect) => ({  connectDragSource: connect.dragSource()}))export default class Resource extends React.Component {  render() {    const {connectDragSource, id, onMove, ...props} = this.props;    return connectDragSource(      <li {...props}>{props.children}</li>    );  }}
+import React from 'react';
+import {DragSource} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const resourceSource = {
+  beginDrag(props) {
+    console.log('begin dragging resource', props);
+    return {};
+  }
+};
+
+@DragSource(ItemTypes.RESOURCE, resourceSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+
+export default class Resource extends React.Component {
+  render() {
+    const {connectDragSource, id, onMove, ...props} = this.props;
+    return connectDragSource(
+        <li {...props}>{props.children}</li>
+        );
+  }
+}
