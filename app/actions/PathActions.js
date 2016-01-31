091a50cc9d87b1ee44f836571317e829dd306346
Bijan <bijanbwb@gmail.com>
2016-01-30 21:40:48 -0500

    Fix grievous mistake in PathActions

--- app/actions/PathActions.js
+++ app/actions/PathActions.js
@@ -1,3 +1,3 @@
 import alt from '../libs/alt';
 
-export default alt.generateActions('create, attachToPath, detachFromPath');
+export default alt.generateActions('create', 'attachToPath', 'detachFromPath');
